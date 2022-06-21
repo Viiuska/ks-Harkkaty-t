38e23416bc2d1a736abd4151aadd89533ee0e31b
Viiuska <87257685+Viiuska@users.noreply.github.com>
2022-06-21 16:44:58 +0300

    Update my-cat.c

--- my-cat.c
+++ my-cat.c
@@ -1,6 +1,26 @@
+#include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, char ** argv) {
-    printf("Hello there!\n");
+#define BUFSIZE 1000
+
+int main (int argc, char *argv[]) {
+    
+    //Tämä sitten for rakenteeseen jos käyttäjä 
+    //haluaa lukea monta tiedotoa yhdellä kertaa
+    
+    FILE *fp = fopen(argv[1], "r");
+    
+    if (fp == NULL) {
+    printf("my-cat: cannot open file\n");
+    exit(1);
+    }
+    char buff[BUFSIZE];
+    
+    while(fgets(buff, BUFSIZE-1, fp)!=NULL){
+        printf("%s", buff);
+    }
+    
+    fclose(fp);
+    
     return 0;
 }
