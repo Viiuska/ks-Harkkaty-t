e9e6cc1ca613d040d32d48bb5f579da32c615dbd
Viiuska <87257685+Viiuska@users.noreply.github.com>
2022-06-22 14:49:22 +0300

    Update my-grep.c

--- my-grep.c
+++ my-grep.c
@@ -1,1 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
+int main (int argc, char *argv[]) {
+
+    if(argc == 1){
+        printf("my-grep: searchterm [file...]\n");
+        exit(1);
+    }
+    
+    for(int i=2; i<argc; i++){
+        FILE *fp = fopen(argv[i], "r");
+            
+        if (fp == NULL) {
+            printf("my-cat: cannot open file\n");
+            exit(1);
+        }
+        
+        char *contents = NULL;
+        size_t len = 0;
+        
+        printf("%s:\n", argv[i]);
+        while(getline(&contents, &len, fp)!=-1){
+            if(strstr(contents , argv[1]) != NULL){
+                printf("%s", contents);
+            }
+        }
+            
+        fclose(fp);
+        free(contents);
+    }
+    return 0;
+}
