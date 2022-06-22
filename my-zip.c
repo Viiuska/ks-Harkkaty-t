ba889283e11cdd734af86e83c247a4b2ab096af4
Viiuska <87257685+Viiuska@users.noreply.github.com>
2022-06-22 16:32:22 +0300

    Update my-zip.c

--- my-zip.c
+++ my-zip.c
@@ -1,1 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
+int main (int argc, char *argv[]) {
+
+    if(argc == 1){
+        printf("my-zip: file1 [file2...]\n");
+        exit(1);
+    }
+    
+    FILE *fp = fopen(argc[1], "r");
+    FILE *fpz = fopen(argc[2], "w");
+    
+    if (fp == NULL){
+                printf("Could not open file\n");
+                exit(-1);
+        }
+    
+    
+    return 0;
+}
