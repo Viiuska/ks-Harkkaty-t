636cd2d6ee6dc2c1f8f8ff04e7566eae97294ba3
Viiuska <87257685+Viiuska@users.noreply.github.com>
2022-06-29 17:47:39 +0300

    Update my-zip.c

--- my-zip.c
+++ my-zip.c
@@ -38,3 +38,53 @@ int main (int argc, char *argv[]) {
     return 0;
 }
 
+
+
+
+//versio 2
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void compress(char *path) {
+    FILE *fp = fopen(path, "rb");
+    char prev = 0, curr = 0;
+    int count = 0;
+    if(fp == NULL){
+        printf("[%s] file does not exist, exiting program ..\n", path);
+        exit(1);
+    }
+    while(1){
+        curr = fgetc(fp);
+        if(curr == EOF){
+            if(count > 0) {
+            fwrite(&count, 1, 4, stdout);
+            fwrite(&prev, 1, 1, stdout);
+        }
+            break;
+        }
+        if(curr != prev) {
+                /* * We got a new character, so first write the count of previous * character. */
+                if(count > 0) {
+                    fwrite(&count, 1, 4, stdout);
+                    fwrite(&prev, 1, 1, stdout);
+                }
+                count = 0;
+        }
+        count++; prev = curr;
+    }
+    fclose(fp);
+
+}
+
+int main(int argc, char **argv) {
+    int i = 0;
+    if(argc < 2) {
+        printf("wzip: file1 [file2 ...]\n");
+        exit(1);
+    }
+    for(i = 1; i < argc; i++) {
+            compress(argv[i]);
+    }
+    return 0;
+}
