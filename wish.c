16b4c29087c4447354e8ec74f926aa9b3db5c6bb
iitaellu <78799868+iitaellu@users.noreply.github.com>
2022-07-06 18:06:09 +0300

    Create wish.c

---
+++ wish.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+#include <stdlib.h>
+
+//Help from https://github.com/tanishqjasoria/wish/blob/master/wish.c
+
+int main(){
+        char * line = NULL;
+        char* input[10];
+        size_t linec = 0;
+        while(1){
+                printf("wish> ");
+
+                if(strcmp(input[0], "exit") == 0){
+                        exit(0);
+                }
+                
+        }
+        return 0;
+}
