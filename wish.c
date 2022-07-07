5282d8519217b5c89e3d23b9835ad18f6877399a
iitaellu <78799868+iitaellu@users.noreply.github.com>
2022-07-07 18:07:15 +0300

    Update wish.c

--- wish.c
+++ wish.c
@@ -2,8 +2,27 @@
 #include<string.h>
 #include <stdlib.h>
 
-//Help from https://github.com/tanishqjasoria/wish/blob/master/wish.c
+//From https://www.geeksforgeeks.org/making-linux-shell-c/
+void execArgs(char** parsed)
+{
+    // Forking a child
+    pid_t pid = fork();
 
+    if (pid == -1) {
+        printf("\nFailed forking child..");
+        return;
+    } else if (pid == 0) {
+        if (execvp(parsed[0], parsed) < 0) {
+        }
+        exit(0);
+    } else {
+        // waiting for child to terminate
+        wait(NULL);
+        return;
+    }
+}
+
+//Help from https://github.com/tanishqjasoria/wish/blob/master/wish.c
 int main(){
         char * line = NULL;
         char* input[10];
@@ -14,6 +33,7 @@ int main(){
                 if(strcmp(input[0], "exit") == 0){
                         exit(0);
                 }
+                execArgs(input);
                 
         }
         return 0;
