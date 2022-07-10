667832f6702fc3f8d09911e0ab74b7bb612d630b
iitaellu <78799868+iitaellu@users.noreply.github.com>
2022-07-10 15:59:17 +0300

    Update wish.c

--- wish.c
+++ wish.c
@@ -61,3 +61,38 @@ int main(){
         }
         return 0;
 }
+
+/*int main(int argc, char **argv){
+        char * line = NULL;
+        char* input[10];
+        size_t linec = 0;
+
+        if (argc == 1){
+
+                while(1){
+                        printf("wish> ");
+
+                        linec = getline(&line, &linec, stdin);
+
+                        //int input_len = string_break(line, input);
+
+                        if(strcmp(input[0], "exit") == 0){
+                                exit(0);
+                        }
+                        if(strcmp(input[0], "cd") == 0){
+                                printf("cd\n");
+                        }if(strcmp(input[0], "path") == 0){
+                                printf("path\n");
+                        }
+                        execArgs(input);
+
+                }
+        }
+        if (argc == 2){
+                printf("Read file\n");
+        }
+        if (argc > 2){
+                printf("Too many arguments\nPromts are './wish' or './wish filename.txt'\n");
+        }
+        return 0;
+}*/
