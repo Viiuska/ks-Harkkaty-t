b5dc0db35e7fb1b3790b55378b65a2d9f6a7f4e2
Viiuska <87257685+Viiuska@users.noreply.github.com>
2022-07-11 15:52:00 +0300

    Update my-zip.c

--- my-zip.c
+++ my-zip.c
@@ -1,48 +1,3 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-
-#define BUFSIZE 1000
-
-int main (int argc, char *argv[]) {
-    if(argc == 1){
-        printf("my-zip: file1 [file2...]\n");
-        exit(1);
-    }
-    
-    FILE *fp = fopen(argv[1], "r");
-    //FILE *fpz = fopen(argc[2], "w");
-    
-    if (fp == NULL){
-                printf("Could not open file\n");
-                exit(-1);
-        }
-    char buff[BUFSIZE];
-    char current=buff[0];
-    int count =1;
-        printf("%s:\n", argv[1]);
-        while(fgets(buff, BUFSIZE-1, fp)!=NULL){
-            for(int i =1; buff[i] != '\0'; i++){
-                if(buff[i] == current){
-                    count++;
-                }else{
-                    printf("%c%d", current,count);
-                    current = buff[i];
-                    count=1;
-                }
-            }
-            
-        }
-        
-        fclose(fp);    
-    return 0;
-}
-
-
-
-
-//versio 2
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -51,20 +6,21 @@ void compress(char *path) {
     char prev = 0, curr = 0;
     int count = 0;
     if(fp == NULL){
-        printf("[%s] file does not exist, exiting program ..\n", path);
+        printf("[%s] file does not exist.\n", path);
         exit(1);
     }
     while(1){
         curr = fgetc(fp);
         if(curr == EOF){
             if(count > 0) {
+            //writes first the character and then number
             fwrite(&count, 1, 4, stdout);
             fwrite(&prev, 1, 1, stdout);
         }
             break;
         }
         if(curr != prev) {
-                /* * We got a new character, so first write the count of previous * character. */
+                //writes first the character and then number
                 if(count > 0) {
                     fwrite(&count, 1, 4, stdout);
                     fwrite(&prev, 1, 1, stdout);
@@ -80,46 +36,7 @@ void compress(char *path) {
 int main(int argc, char **argv) {
     int i = 0;
     if(argc < 2) {
-        printf("wzip: file1 [file2 ...]\n");
-        exit(1);
-    }
-    for(i = 1; i < argc; i++) {
-            compress(argv[i]);
-    }
-    return 0;
-}
-
-
-
-
-//säätöä menossa 3 versio kesken
-
-#include <stdio.h>
-#include <stdlib.h>
-
-void compress(char *filename) {
-    //char prev;
-    char curr;
-    //int count = 0;
-    //char str[60];
-    
-    FILE *fp = fopen(filename, "rb");
-    
-    if(fp == NULL){
-        printf("[%s] file does not exist, exiting program ..\n", filename);
-        exit(1);
-    }
-    
-    curr = fgetc(fp);
-    printf("%c",curr);
-
-    fclose(fp);
-}
-
-int main(int argc, char **argv) {
-    int i = 0;
-    if(argc < 2) {
-        printf("wzip: file1 [file2 ...]\n");
+        printf("my-zip: file1 [file2 ...]\n");
         exit(1);
     }
     for(i = 1; i < argc; i++) {
